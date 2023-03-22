ce0fc5b6ed83ceb190c7c875b61ea15c83e91727
Sami Nafis <78338492+sodiumfishh@users.noreply.github.com>
2023-03-22 12:46:59 +0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
-<template>
-  <h1>Ninja reaction timer</h1>
-  <button @click="start" :disabled="isPlaying" class="playBtn">Play</button>
-  <Results v-if="score" :score="score" />
-  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-</template>
+<template></template>
 
 <script>
-import Block from "./components/Block.vue"
-import Results from "./components/Results.vue"
-
 export default {
   name: 'App',
-  components: {
-    Block,
-    Results
-  },
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-      score: null,
-    }
-  },
-  methods: {
-    start() {
-      this.delay = 2000 + Math.random() * 5000
-      this.isPlaying = true
-      this.score = null
-    },
-    endGame(reactionTime) {
-      this.score = reactionTime
-      this.isPlaying = false
-    }
-  },
+  components: {},
 }
 </script>
 
@@ -44,19 +15,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.playBtn {
-  color: #fff;
-  background: rgba(44, 156, 113, 1);
-  border: none;
-  padding: 0.5rem 1rem;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.playBtn:disabled {
-  background: rgba(44, 156, 113, 0.2);
-  cursor: not-allowed;
 }
 </style>
